9d9582adffb950d939aa42fa60412b1fd20fa16d
Roshan <107319136+roshan798@users.noreply.github.com>
2023-01-17 23:47:37 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function toggleNavbar() {
+    const nav = document.getElementById("navbar");
+    if(nav.style.display == "none"){
+        nav.style.display = "block";
+    }
+    else{ 
+        nav.style.display = "none";
+    }
+
+}
