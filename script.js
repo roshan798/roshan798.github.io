4174a2c2af419b0909a313193b71abe0a4e3d678
Roshan <107319136+roshan798@users.noreply.github.com>
2023-01-24 20:05:15 +0530

    Added Some Transitions

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 
 function toggleNavbar() {
     const nav = document.getElementById("navbar");
+    const toggleIMG = document.getElementById("toggleIMG");
     if(nav.style.display == "none"){
         nav.style.display = "block";
+        toggleIMG.style.display = "none";
     }
     else{ 
         nav.style.display = "none";
+        toggleIMG.style.display = "block";
     }
 
 }
